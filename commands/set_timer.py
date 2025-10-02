72b568fb157895100e69b51c636cb58d5b7128c5
Hussain fathy <Hussainfathy911@gmail.com>
2025-10-02 16:59:26 +0300

    update this file for the changes that have been made in the set_monitoring_topics file, and add the start command and the logging in a good way, and import the make_new_tabl to make sure that they are there if a new person uses the code

--- commands/set_timer.py
+++ commands/set_timer.py
@@ -9,7 +9,15 @@ async def weekly_check(context : ContextTypes.DEFAULT_TYPE):
     job = context.job
     chat_id = job.chat_id
     DM().weekly_missed_update(chat_id)
-    await context.bot.send_message(chat_id, text="تم عمل التشيك الأسبوعي ✅")
+    app_info = await context.bot.get_me()
+    bot_link = f"https://t.me/{app_info.username}?start=weekly_sub"
+    msgg = "مرحبًا يا أبطال! حبيت أفكركم إن أسبوع جديد بدء و الوقت حان عشان تشاركوا إنجازاتكم الأسبوعية 📝\n"
+    msg = await context.bot.send_message(
+    chat_id,
+    text=msgg + f'👋 وعشان توصلك التنبيهات في الخاص اضغط <a href="{bot_link}">اشتراك</a>',
+    parse_mode="HTML")
+    await context.bot.pin_chat_message(chat_id,msg.id)
+
     
 async def set_timer(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.effective_chat.id
@@ -20,9 +28,8 @@ async def set_timer(update: Update, context: ContextTypes.DEFAULT_TYPE):
             time=datetime.time(tzinfo=ZoneInfo("Africa/Cairo")),  
             days=(6,),  
             name=str(chat_id),                   
-            chat_id=chat_id,
-            
+            chat_id=chat_id,          
         )
-        await update.message.reply_text("تم تفعيل التشيك الأسبوعي ✅")
+        await update.message.reply_text("تم تفعيل التذكير الأسبوعي ✅")
     else:
         await update.message.reply_text("❌ ليس لديك صلاحية")
--- main.py
+++ main.py
@@ -1,25 +1,37 @@
-from telegram import Update ,InlineQueryResultArticle, InputTextMessageContent
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters,InlineQueryHandler
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
 from data_manager import Data_Manager as DB , Bot_Setting as BS
 import os
 from commands.set_group import set_group
-from commands.set_monitoring_topic import set_monitoring_topic
+from commands.set_monitoring_topics import study_monitoring_topic , weekly_monitoring_topic
 from commands.set_notification_topic import set_notification_topic
 from commands.set_rules_topic import set_rules_topic
 from commands.massage_handler import monitoring_topic , new_user
 from commands.state import state
 from commands.set_timer import set_timer
 from commands.restart_missed import restart_missed
+from commands.start import start
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 import logging
 
-# تعطيل اللوجات من مكتبة httpx
+logging.basicConfig(
+    level=logging.DEBUG, 
+    format="%(asctime)s - %(levelname)s - %(message)s",  
+    datefmt="%Y-%m-%d %H:%M:%S"  
+)
+
 logging.getLogger("httpx").setLevel(logging.WARNING)
-    
+
+DB().make_new_table()
+BS().make_new_table()
+
+
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 
+
+app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("set_group", set_group))
-app.add_handler(CommandHandler("set_monitoring_topic", set_monitoring_topic))
+app.add_handler(CommandHandler("study_monitoring_topic", study_monitoring_topic))
+app.add_handler(CommandHandler("weekly_monitoring_topic", weekly_monitoring_topic))
 app.add_handler(CommandHandler("set_notification_topic", set_notification_topic))
 app.add_handler(CommandHandler('set_rules_topic', set_rules_topic))
 app.add_handler(CommandHandler('restart_missed', restart_missed))
