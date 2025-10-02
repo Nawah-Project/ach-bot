426a6e503c214487623b459ae8bdef20b67c12e1
Hussain fathy <Hussainfathy911@gmail.com>
2025-10-02 17:14:59 +0300

    update some logic that will help to register the user who will come from the link and subscribe to the bot

--- commands/set_timer.py
+++ commands/set_timer.py
@@ -4,21 +4,21 @@ from data_manager import Data_Manager as DM
 from utils import is_admin
 import  datetime
 from zoneinfo import ZoneInfo
-
+import logging
 async def weekly_check(context : ContextTypes.DEFAULT_TYPE):
     job = context.job
     chat_id = job.chat_id
     DM().weekly_missed_update(chat_id)
     app_info = await context.bot.get_me()
-    bot_link = f"https://t.me/{app_info.username}?start=weekly_sub"
+    bot_link = f"https://t.me/{app_info.username}?start=join_{chat_id}"
     msgg = "مرحبًا يا أبطال! حبيت أفكركم إن أسبوع جديد بدء و الوقت حان عشان تشاركوا إنجازاتكم الأسبوعية 📝\n"
     msg = await context.bot.send_message(
     chat_id,
     text=msgg + f'👋 وعشان توصلك التنبيهات في الخاص اضغط <a href="{bot_link}">اشتراك</a>',
     parse_mode="HTML")
     await context.bot.pin_chat_message(chat_id,msg.id)
+    logging.info(f"weekly_check done")
 
-    
 async def set_timer(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.effective_chat.id
     user_id = update.effective_user.id
@@ -30,6 +30,8 @@ async def set_timer(update: Update, context: ContextTypes.DEFAULT_TYPE):
             name=str(chat_id),                   
             chat_id=chat_id,          
         )
+        
         await update.message.reply_text("تم تفعيل التذكير الأسبوعي ✅")
+        logging.info(f"set_timer done")
     else:
         await update.message.reply_text("❌ ليس لديك صلاحية")
